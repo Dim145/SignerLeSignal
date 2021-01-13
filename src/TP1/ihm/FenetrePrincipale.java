1b5e45f4127533c377de9bd342bf60b27c88d5a2
bf180293 <443-bf180293@users.noreply.www-apps.univ-lehavre.fr>
2021-01-13 12:30:56 +0100

    Ajout de trucs

--- src/TP1/ihm/FenetrePrincipale.java
+++ src/TP1/ihm/FenetrePrincipale.java
@@ -4,8 +4,11 @@ import javax.swing.*;
 
 public class FenetrePrincipale extends JFrame
 {
+	JPanel pnlHaut;
     public FenetrePrincipale()
     {
-
+    	this.setTitle("Projet Boireau/Dubois");
+    	this.setSize(500, 500);
+    	pnlHaut = new JPanel();
     }
 }
