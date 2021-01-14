2538bc1ae8154385f8472c6a50a8cb975d827771
bf180293 <443-bf180293@users.noreply.www-apps.univ-lehavre.fr>
2021-01-14 13:09:11 +0100

    modification du add a git

---
+++ src/TP1/ihm/PanelDessin.java
@@ -0,0 +1,18 @@
+package TP1.ihm;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class PanelDessin extends JPanel
+{
+	private int setup;
+	
+	public PanelDessin(int setup)
+	{
+		this.setup = setup;
+		Graphics2D g2 = new Gra
+	}
+}
